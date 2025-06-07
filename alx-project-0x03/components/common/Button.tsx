13315de3c715f0b4da410112c7b5ced01748d09d
Vegas90 <amosmwoni@gmail.com>
2025-06-07 23:14:59 +0300

    m

--- alx-project-0x03/components/common/Button.tsx
+++ alx-project-0x03/components/common/Button.tsx
@@ -1,10 +1,4 @@
-
-interface ButtonProps {
-  buttonLabel: string
-  buttonSize?: string
-  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
-  action?: () => void
-}
+import {ButtonProps} from '@/interfaces/index';
 
 
 const Button = ({ buttonLabel, buttonSize, buttonBackgroundColor, action }: ButtonProps) => {
